7b1897885f8d2ac0d40ca781f7258847eebe599b
miharadeskaa <sillimika@hotmail.com>
2026-02-07 10:47:27 +0100

    new

--- TestBank/TestDeMaClasse.cs
+++ TestBank/TestDeMaClasse.cs
@@ -15,4 +15,42 @@ public class TestDeMaClasse
         // Assert
         Assert.Equal(expected: "Résultat attendu", actual: result);
     }
+
+    
+// Tests unitaires pour vérifier les fonctionnalités du depot et de retrait 
+
+    [Fact]
+    public void TestDepotEtRetrait()
+    {
+        // Arrange
+        var compte = new CompteBancaire("Mon Compte", 1000);
+        var montantDepot = 500;
+        var montantRetrait = 200;
+
+        // Act
+        compte.Deposer(montantDepot);
+        compte.Retirer(montantRetrait);
+
+        // Assert
+        Assert.Equal(expected: 1300, actual: compte.Solde);
+    }
+
+
+// Verifier que le virement fonctionne entre les deux comptes 
+    [Fact]
+    public void TestVirement()
+    {
+        // Arrange
+        var compteA = new CompteBancaire("Compte A", 1000);
+        var compteB = new CompteBancaire("Compte B", 500);
+        var montantVirement = 200;
+
+        // Act
+        compteA.Virer(compteB, montantVirement);
+
+        // Assert
+        Assert.Equal(expected: 800, actual: compteA.Solde);
+        Assert.Equal(expected: 700, actual: compteB.Solde);
+    }   
+
 }
